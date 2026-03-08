c48dda94d8395e78b90af5a6fa272875f5d17f62
Alexander Kim <limosha@inbox.ru>
2026-03-09 02:19:09 +0300

    fix(core): fix tls setting

--- src/services/email_sender.py
+++ src/services/email_sender.py
@@ -121,7 +121,6 @@ async def send_email(to: str, subject: str, html: str, text: str) -> bool:
                     hostname="smtp.gmail.com",
                     port=587,
                     start_tls=True,
-                    use_tls=True,
                     username=GMAIL_USER,
                     password=GMAIL_PASS,
                     timeout=SMTP_TIMEOUT
